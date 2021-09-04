93eee109882d9673eb23d46fa0ecb1893b50af26
valyukhov <alexzed@bk.ru>
2021-09-04 07:04:53 +0300

    second

---
+++ src/components/App/App.jsx
@@ -0,0 +1,38 @@
+import  { useEffect, useState } from 'react';
+import Search from '../Search';
+
+import List from '../List';
+
+
+
+import './App.css';
+
+console.log('List', List);
+console.log('Search', Search);
+const data =[
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+];
+function App() {
+const [search, setSearch] = useState('');
+const [items, setItems] = useState(data);
+
+useEffect(() => {
+  setItems(data.filter(el => el.toLowerCase().includes(search.toLowerCase())));
+}, [search])
+
+  return (
+    <div className="App">
+      <div className="App-header">
+      <Search value={search} onChange={(e) => setSearch(e.target.value)}>
+        Find course:
+      </Search>
+        <List items={items} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/App/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () =>{
+  it('App renders', ()=>{
+    render (<App />);
+    // screen.debug();
+    expect(screen.getByRole('list')).toBeInTheDocument();
+  })
+})

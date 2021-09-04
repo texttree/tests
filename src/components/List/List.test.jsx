75d5de5b849aeadcc38ffd2b0252b9768184fc2d
valyukhov <alexzed@bk.ru>
2021-09-04 07:31:29 +0300

    add comments to List

--- src/components/List/List.test.jsx
+++ src/components/List/List.test.jsx
@@ -5,12 +5,12 @@ import List from './List';
 const data = ['html', 'css', 'js']
 
 
-describe('List component', ()=>{
-    it('List renders',() => {
-        render(<List items ={data}/>);
+describe('List component', ()=>{ //как правило пишется блок тестов, в которых уже пишутся тесты
+    it('List renders',() => { //можно it, можно test, в кавычках указываются суть теста
+        render(<List items ={data}/>);//render тестирует отрисовку компонента при определенных пропсах
 
-        expect(screen.getByRole('list')).toBeInTheDocument();
-        expect(screen.getByText('html')).toBeInTheDocument();
+        expect(screen.getByRole('list')).toBeInTheDocument();//screen - это уже нарисованный рендер, в нем проверяют на существование отдельных элементов 
+        expect(screen.getByText('html')).toBeInTheDocument();//expect - это метод, который тестирует то, что мы ожидаем / предполагаем от рендера/функции. Если наши ожидания не оправдались, тест провалится
     });
     it('List renders without data',() => {
         render(<List />);

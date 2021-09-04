16f4e51c02536abb6a7f69c6be0e4aaa7ab0536e
valyukhov <alexzed@bk.ru>
2021-09-04 07:27:55 +0300

    add comments

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('App component', () =>{
-  it('App renders', ()=>{
-    render (<App />);
-    //  screen.debug();
-    expect(screen.getByRole('list')).toBeInTheDocument();
+describe('App component', () =>{  //как правило пишется блок тестов, в которых уже пишутся тесты
+  it('App renders', ()=>{ //можно it, можно test, в кавычках указываются суть теста
+    render (<App />); //render тестирует отрисовку компонента при определенных пропсах
+    //  screen.debug(); // если это запустить, можно увидеть в тестах всю структуру документа
+    expect(screen.getByRole('list')).toBeInTheDocument();//screen - это уже нарисованный рендер, в нем проверяют на существование отдельных элементов 
   })
 })
